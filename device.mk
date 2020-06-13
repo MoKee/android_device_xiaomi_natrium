e85d84b33f121580d9b52fdd8126e8434be35c71
Martincz Gao <martincz@mokeedev.com>
2020-06-13 13:44:36 +0800

    natrium: mokee-ify

--- device.mk
+++ device.mk
@@ -23,10 +23,10 @@ $(call inherit-product, vendor/xiaomi/natrium/natrium-vendor.mk)
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-mokee/mokee-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -68,7 +68,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8996
+    vendor.mokee.touch@1.0-service.xiaomi_8996
 
 # Thermal
 PRODUCT_COPY_FILES += \
